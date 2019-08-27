1d162d00807dfde19f83155cb4c30b647fa73755
ken <kenmugy@gmail.com>
2019-08-27 14:48:53 +0300

    refactored application by removing state from that App component and handing it iver to the clock component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Clock from './Clock';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      date: new Date().toLocaleString()
-    };
-  }
-
-  componentDidMount() {
-    this.interval = setInterval(() => {
-      this.setState({
-        date: new Date().toLocaleString()
-      });
-    });
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  render() {
-    return (
-      <div>
-        <Clock date={this.state.date} />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <Clock />
+    </div>
+  );
+};
 
 export default App;
--- src/Clock.js
+++ src/Clock.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Clock = ({ date }) => {
-  return <div className='clock'>{date}</div>;
-};
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date().toLocaleString()
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        date: new Date().toLocaleString()
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return <div className='clock'>{this.state.date}</div>;
+  }
+}
 
 export default Clock;
