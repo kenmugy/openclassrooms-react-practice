16d5838f22246c7ed14aafbb2e6cbdc4683a329c
ken <kenmugy@gmail.com>
2019-08-27 14:37:44 +0300

    added Clock component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
+import Clock from './Clock';
+import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      date: new Date().toLocaleString()
+    };
   }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        date: new Date().toLocaleString()
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Clock date={this.state.date} />
+      </div>
+    );
   }
 }
 
---
+++ src/Clock.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Clock = ({ date }) => {
+  return <div className='clock'>{date}</div>;
+};
+
+export default Clock;
